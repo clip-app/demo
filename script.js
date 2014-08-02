d657d55f734f10e41d89811527efcc63deeb381d
Taylor Blau <me@ttaylorr.com>
2014-08-02 14:06:14 -0700

    fix shit

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
-$(function() {
-  var done = false;
-  var player;
-  function onYouTubeIframeAPIReady() {
-      player = new YT.Player('#player', {
-        height: '390',
-        width: '640',
-        videoId: 'PQBTd4xF6tU',
-        events: {
-          'onReady': onPlayerReady,
-          'onStateChange': onPlayerStateChange
-        }
-      });
-  }
-  function onPlayerReady(evt) {
-      evt.target.playVideo();
-  }
-  function onPlayerStateChange(evt) {
-      if (evt.data == YT.PlayerState.PLAYING && !done) {
-          setTimeout(stopVideo, 6000);
-          done = true;
+var done = false;
+var player;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+      height: '390',
+      width: '640',
+      videoId: 'PQBTd4xF6tU',
+      events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
       }
-  }
-  function stopVideo() {
-      player.stopVideo();
-  }
-});
+    });
+};
+
+function onPlayerReady(evt) {
+    evt.target.playVideo();
+};
+
+function onPlayerStateChange(evt) {
+    if (evt.data == YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 6000);
+        done = true;
+    }
+}
+
+function stopVideo() {
+    player.stopVideo();
+};
