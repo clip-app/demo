7a548dd1a4c0ab934b59da9368ef2ab96d7ddfc8
Willium <w@ml.to>
2014-08-02 14:02:55 -0700

    p

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-$('#click').click(function() {
-	$('#video').append('hi<br/>');
-})
+$(function() {
+  var done = false;
+  var player;
+  function onYouTubeIframeAPIReady() {
+      player = new YT.Player('#player', {
+        height: '390',
+        width: '640',
+        videoId: 'PQBTd4xF6tU',
+        events: {
+          'onReady': onPlayerReady,
+          'onStateChange': onPlayerStateChange
+        }
+      });
+  }
+  function onPlayerReady(evt) {
+      evt.target.playVideo();
+  }
+  function onPlayerStateChange(evt) {
+      if (evt.data == YT.PlayerState.PLAYING && !done) {
+          setTimeout(stopVideo, 6000);
+          done = true;
+      }
+  }
+  function stopVideo() {
+      player.stopVideo();
+  }
+});
