5bc8adddef21cfa14520e283fb87a159d2c05b6a
Taylor Blau <me@ttaylorr.com>
2014-08-02 17:11:54 -0700

    asdf

--- script.js
+++ script.js
@@ -23,11 +23,12 @@ function formVideoId(videoId) { return 'player-' + videoId; };
 // start-point
 function onYouTubeIframeAPIReady() {
   var $players = $('#players');
-
+  var index = -1;
   var base_events = {'onReady': onPlayerReady};
 
   examples.forEach(function(item) {
     // generate video div
+    item.i = ++index;
     $players.append($("<div>", {id: formVideoId(item.videoId), 'data-start': item.startTime, 'data-end': item.endTime}));
 
     players.push({meta: item, video: new YT.Player(formVideoId(item.videoId), {
@@ -42,25 +43,38 @@ function onYouTubeIframeAPIReady() {
   });
 };
 
+function findMetaPair(videoId) {
+  for (var i = 0; i < players.length; i++) {
+    if (players[i].meta.videoId == videoId) {
+      return players[i];
+    }
+  }
+}
+
 function onPlayerReady(e) {
   // store off the videoid
   var videoId = e.target.o.videoData.video_id;
 
-  var index;
-  for (var i = 0; i < players.length; i++) {
-    var playerHash = players[i];
-    if (playerHash.video == e.target) {
-      playerHash.video.cueVideoById(videoId, playerHash.meta.startTime);
-      playerHash.video.playVideo();
-      playerHash.video.stopVideo();
-      index = i;
-      break;
+  var pair = findMetaPair(videoId);
+  var video = pair.video;
+
+  // start buffering that shit up
+  video.addEventListener('onStateChange', cueFunc);
+  video.cueVideoById(videoId, pair.meta.startTime);
+}
+
+var cueFunc = function(e) {
+  console.log(e.data);
+  if (e.data == 1) {
+    e.target.removeEventListener('onStateChange', cueFunc);
+    e.target.pauseVideo();
+ 
+    console.log(findMetaPair(e.target.o.videoData.video_id).meta.index);
+    if (findMetaPair(e.target.o.videoData.video_id).meta.index == examples.length - 1) {
+      console.log('last1');
     }
   }
-
-  var last = index + 1 == players.length;
-  if (last) beginAll();
-}
+};
 
 function beginAll() {
   var currentTime = 0;
